453eac1a2dd9c864f4d7948668bb755e0f174e4b
UntouchableOvotschsch <sergey.post@list.ru>
2023-10-06 04:10:46 +0300

    upgrade packages, fix getArticlesTypes selector

--- src/pages/ArticlesPage/model/selectors/getArticlesActiveTypes/index.ts
+++ src/pages/ArticlesPage/model/selectors/getArticlesActiveTypes/index.ts
@@ -1,4 +1,10 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 import { StateSchema } from '@/app/providers/StoreProvider';
 import { ArticleTypes } from '@/entities/Article';
 
-export const getArticlesActiveTypes = (state: StateSchema) => state?.articlesPage?.types || [ArticleTypes.ALL];
+// Иначе стреляет ошибкой о том, что при одинаковых входных данных селектор возвращает разные значения
+export const getArticlesActiveTypes = createSelector(
+    (state: StateSchema) => state?.articlesPage?.types,
+    (types) => (types?.length ? types : [ArticleTypes.ALL]),
+);
